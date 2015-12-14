ea0722e32448a1a13d8e9b44c9a10aa4821aed15
TomasW <tomas.widlak@gmail.com>
2015-12-14 09:33:02 +0100

    edit example

--- example/index.php
+++ example/index.php
@@ -1,8 +1,14 @@
 <?php
 
+spl_autoload_register(function($className) {
+    $className = str_replace("MonkeyData\\EshopXmlFeedGenerator\\", "", $className);
+    $className = str_replace("\\", DIRECTORY_SEPARATOR, $className);
+    include_once '..'.DIRECTORY_SEPARATOR."src".DIRECTORY_SEPARATOR.$className . ".php";
+    }
+);
+
 require_once __DIR__ . '/MonkeyDataXmlModel.php';
 require_once __DIR__ . '/MonkeyDataExampleXmlGenerator.php';
-
 
 $xmlGenerator = new MonkeyDataExampleXmlGenerator();
 $xmlGenerator->run();
